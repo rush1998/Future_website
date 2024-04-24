67d785b49f9d43967e0776998bedd250c7b29679
rush1998 <rushabh.m.patel1998@gmail.com>
2024-04-23 20:16:12 -0400

    changes in app.js file

--- app.js
+++ app.js
@@ -4,12 +4,6 @@ const menuLinks = document.querySelector('.navbar__menu');
 menu.addEventListener('click', function() {
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
-});
 
-const addToCartBtn = document.querySelector('.add-to-cart-btn');
-addToCartBtn.addEventListener('click', () => {
-  // Simulate adding the product to the cart (you can replace this with your actual logic)
-  // For demonstration purposes, let's just navigate to the cart page
-  window.location.href = 'cart.html'; // Replace 'cart.html' with your actual cart page URL
 });
 
